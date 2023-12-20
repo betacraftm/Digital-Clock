3ac8e2228e09c3f48709e9f6628838ee16e00091
Betacraft <hoangkimc9@gmail.com>
2023-12-20 20:38:17 +0700

    Import dayjs library

--- script.js
+++ script.js
@@ -1,43 +1,11 @@
-window.onload = init();
+import dayjs from "https://cdn.jsdelivr.net/npm/dayjs@1.11.10/+esm";
 
-function init() {
+window.addEventListener("DOMContentLoaded", () => {
   setInterval(getTime, 50);
-
   function getTime() {
-    const currentDate = new Date();
-    const options = { weekday: "short" };
-    const day = document.querySelector(".day");
-    const weekDay = document.querySelector(".week-day");
-    const month = document.querySelector(".month");
-    const year = document.querySelector(".year");
-    const hour = document.querySelector(".hour");
-    const minute = document.querySelector(".minute");
-    const second = document.querySelector(".second");
-
-    //generate date
-
-    weekDay.innerHTML = currentDate.toLocaleDateString("en-US", options);
-    month.innerHTML = currentDate.getMonth() + 1;
-    day.innerHTML = currentDate.getDate();
-    year.innerHTML = currentDate.getFullYear();
-
-    //adjust current time
-    if (currentDate.getHours() >= 0 && currentDate.getHours() < 10) {
-      hour.innerHTML = `0${currentDate.getHours()}`;
-    } else {
-      hour.innerHTML = currentDate.getHours();
-    }
-
-    if (currentDate.getMinutes() >= 0 && currentDate.getMinutes() < 10) {
-      minute.innerHTML = `0${currentDate.getMinutes()}`;
-    } else {
-      minute.innerHTML = currentDate.getMinutes();
-    }
-
-    if (currentDate.getSeconds() >= 0 && currentDate.getSeconds() < 10) {
-      second.innerHTML = `0${currentDate.getSeconds()}`;
-    } else {
-      second.innerHTML = currentDate.getSeconds();
-    }
+    const currentTime = document.querySelector(".current-time");
+    const currentDay = document.querySelector(".current-day");
+    currentDay.innerHTML = dayjs().format("dddd DD-MM-YYYY");
+    currentTime.innerHTML = dayjs().format("HH:mm:ss");
   }
-}
+});
